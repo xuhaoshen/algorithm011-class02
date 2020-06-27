c707d66c2fea79cde4f5a3d25bdfe6fb1df5712b
shenxuhao <shenxuhao@9tongmail.com>
2020-06-27 09:28:28 +0800

    week01's homework

--- Week_01/leetcode42_trap/Solution.java
+++ Week_01/leetcode42_trap/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
      *           *
      *    *      * *   *
      *  * * *  * * * * * *
-     *  l                r
+     * l                 r
      * @param height
      * @return
      */
@@ -19,8 +19,8 @@ public class Solution {
         int res = 0;
         int leftMax = 0;
         int rightMax = 0;
-        // 蓄水由最矮挡板决定
         while (left < right) {
+            // 蓄水由最矮挡板决定
             if(height[left] < height[right]) {
                 leftMax = Math.max(leftMax, height[left]);
                 res += (leftMax - height[left]);
@@ -35,30 +35,10 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-//        Solution solution = new Solution();
-//        int[] arr = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-//        int res = solution.trap(arr);
-//        System.out.println(res);
-        System.out.println(11>>>1);
-        PriorityQueue queue = new PriorityQueue();
-
-        queue.offer(14);
-        queue.offer(7);
-        queue.offer(12);
-        queue.offer(6);
-        queue.offer(9);
-        queue.offer(4);
-        queue.offer(17);
-        queue.offer(23);
-        queue.offer(10);
-        queue.offer(15);
-        queue.offer(3);
-        System.out.println(queue);
-
-        int x = 3;
-        int e =9;
-        Comparable<Integer> comparator = (Comparable<Integer>) x;
-        System.out.println(comparator.compareTo(e) >= 0);
+        Solution solution = new Solution();
+        int[] arr = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int res = solution.trap(arr);
+        System.out.println(res);
     }
 
 }
